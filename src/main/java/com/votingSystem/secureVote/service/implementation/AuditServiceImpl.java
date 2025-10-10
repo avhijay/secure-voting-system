23850687cb1a054713f2741a0123f44695bba6e3
avhijay <parmaravhijay5049@gmail.com>
2025-10-11 02:02:12 +0530

    implemented transactional SHA-256 hash chaining for logs

--- src/main/java/com/votingSystem/secureVote/service/implementation/AuditServiceImpl.java
+++ src/main/java/com/votingSystem/secureVote/service/implementation/AuditServiceImpl.java
@@ -6,9 +6,14 @@ import com.votingSystem.secureVote.repository.AuditRepository;
 import com.votingSystem.secureVote.repository.UserRepository;
 import com.votingSystem.secureVote.service.AuditService;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.sql.Timestamp;
 import java.time.LocalDateTime;
+import java.util.HexFormat;
 import java.util.List;
 @Service
 public class AuditServiceImpl implements AuditService {
@@ -24,23 +29,54 @@ public class AuditServiceImpl implements AuditService {
 
 
 
+    
 
 
-
+@Transactional
     @Override
     public Audit logAction(Long userId, String action, String status, String reason) {
-        Users theUser = userRepository.findById(userId).orElseThrow(()-> new RuntimeException("Invalid Id "+userId));
-Audit newAudit= new Audit();
-newAudit.setId(userId);
+
+Audit lastAudit= auditRepository.findTopByOrderByIdDesc();
+String prevHash;
+if(lastAudit==null){
+    prevHash="GENESIS"; // if no previous hash isThere ( in short if it's the first audit log )
+}else{
+    prevHash=lastAudit.getEntryHash(); // for linking to previous hash
+}
+
+   ;
+
+
+
+Audit newAudit = new Audit();
+
+Users theUser = userRepository.findById(userId).orElseThrow(()-> new RuntimeException("Invalid Id "+userId));
+
+newAudit.setUser(theUser);
 newAudit.setAction(action);
 newAudit.setStatus(status);
 newAudit.setReason(reason);
 newAudit.setCreatedAt(Timestamp.valueOf(LocalDateTime.now()));
-
+newAudit.setPrevHash(prevHash);
+String data = prevHash +"|"+ userId +"|"+ action +"|"+ status +"|"+ reason +"|"+Timestamp.valueOf(LocalDateTime.now());
+newAudit.setEntryHash(computeHash(data));
 return auditRepository.save(newAudit);
 
-
     }
+
+
+
+     private  String computeHash(String input){
+        try {
+            MessageDigest digest = MessageDigest.getInstance("SHA-256");
+            byte[] encoded = digest.digest(input.getBytes(StandardCharsets.UTF_8));
+            String hex = HexFormat.of().formatHex(encoded);
+            return hex;
+        }catch(NoSuchAlgorithmException e){
+            throw  new RuntimeException("SHA-256 algorithm not found",e);
+
+         }
+     }
 
     @Override
     public List<Audit> getAllAuditLogs() {
