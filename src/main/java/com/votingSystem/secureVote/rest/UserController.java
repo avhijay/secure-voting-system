24a0eb85930bacfa909780f363eab1858258b58a
avhijay <parmaravhijay5049@gmail.com>
2025-10-21 01:37:14 +0530

    (userCintroller) : exposed endpoints

--- src/main/java/com/votingSystem/secureVote/rest/UserController.java
+++ src/main/java/com/votingSystem/secureVote/rest/UserController.java
@@ -8,14 +8,12 @@ import com.votingSystem.secureVote.entity.Users;
 import com.votingSystem.secureVote.service.UserService;
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
 import java.sql.Timestamp;
 import java.time.LocalDateTime;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/users")
@@ -44,4 +42,48 @@ public class UserController {
         return ResponseEntity.created(location).body(response);
 
     }
+
+    @GetMapping("/{id}")
+    public  ResponseEntity<UserResponse>searchById(@PathVariable Long id){
+        Users user = userService.getUserById(id);
+        UserResponse newResponse = new UserResponse();
+        newResponse.setEmailId(user.getEmail());
+        newResponse.setRole(user.getRole());
+        newResponse.setStatus(user.getStatus());
+        newResponse.setClearanceLevel(user.getClearanceLevel());
+        newResponse.setName(user.getName());
+
+        return  ResponseEntity.ok(newResponse);
+    }
+
+
+    @GetMapping("/{id}")
+    public  ResponseEntity<UserResponse>searchByIdentityKey(@PathVariable String id){
+        Users user = userService.getUserByIdentityKey(id);
+        UserResponse newResponse = new UserResponse();
+        newResponse.setEmailId(user.getEmail());
+        newResponse.setRole(user.getRole());
+        newResponse.setStatus(user.getStatus());
+        newResponse.setClearanceLevel(user.getClearanceLevel());
+        newResponse.setName(user.getName());
+
+        return  ResponseEntity.ok(newResponse);
+    }
+
+    @GetMapping("/{status}")
+    public  ResponseEntity<List<Users>>searchByStatus(@PathVariable String status){
+        List<Users> user = userService.getUserByStatus(status);
+
+
+        return  ResponseEntity.ok(user);
+    }
+    @GetMapping("/{role}")
+    public  ResponseEntity<List<Users>>getByRole(@PathVariable String role){
+        List<Users> user = userService.getUserByStatus(role);
+
+
+        return  ResponseEntity.ok(user);
+    }
+
+
 }
